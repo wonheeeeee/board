0b522501e9df2d3b781684411305ed6317b17722
wonheeeeee <1004hee96@naver.com>
2020-08-27 17:35:53 +0900

    pr1

--- board/settings.py
+++ board/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yjv%&z_n5jj)igmu2vtj70!mwya*95w*)yfcp#kq+%oiw4c#t9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
